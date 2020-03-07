f984de8102db7c88889f50e8f6a1ac5bf36b4cba
NovaelyRachman <43128781+NovaelyRachman@users.noreply.github.com>
2020-03-07 20:58:33 +0800

    Add files via upload
    Menggunakan javascript sudah dicoba di https://codepen.io/pen/

---
+++ 3.js
@@ -0,0 +1,15 @@
+var name = ["D", "U", "M", "B", "W", "A", "Y", "S", "I", "D"];
+var sem = name.length;
+var output = "";
+for (var i = 0; i < sem; i++) {
+    output = "";
+    for (var a = 0; a < sem; a++) {
+        sem[a] = "=";
+    }
+    sem[i] = name[i];
+    sem[temp - 1 - i] = name[i];
+    for (var n = 0; n < sem; n++) {
+        output += sem[n];
+    }
+    console.log(output);
+}
